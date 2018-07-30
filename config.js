5e4a20b42e532adabc16ecce82ad54e6201ac60b
Lars <lars.vlyminckx@hotmail.com>
2018-07-30 18:54:41 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // You can add your own open weather map token here (https://home.openweathermap.org/api_keys)
+    OPENWEATHER_TOKEN: process.env.OPENWEATHER_TOKEN || 'b1ff021a053ddd05e7e6ed08cf90375f',
+    PORT: process.env.PORT || 5000,
+};
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const config = require('./config');
+const loadWeatherRoute = require('./weather');
+
+const app = express();
+app.use(bodyParser.json());
+
+loadWeatherRoute(app);
+
+app.get('/', (req, res) => res.send('Hello World!<br><h1>Hello Weather bot</h1>'))
+
+app.post('/errors', function(req, res) {
+    console.log(req.body);
+    res.sendStatus(200);
+});
+
+
+const port = config.PORT;
+app.listen(port, function() {
+    console.log(`App is listening on port ${port}`);
+});
